db18b72ddf98ea9fde207abc76845a1f3d242cb3
Jarek Machaj <jarekmachaj@users.noreply.github.com>
2019-04-14 01:05:31 +0200

    params

--- devices/customActions/wolService.js
+++ devices/customActions/wolService.js
@@ -1,6 +1,6 @@
 var wol = require('wake_on_lan');
 
 module.exports.wake = (resultCallback, params) =>  { 
-  wol.wake(params.macAddress);
+  wol.wake(params.macAddress, {address : params.ipAddress});
   resultCallback({result : 'success'});
 }
